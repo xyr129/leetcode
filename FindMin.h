3bdbbeb6b6641205d0a40b4d017d0f1903c9c28d
xyr129 <xyr129@126.com>
2016-06-24 16:09:46 +0800

    旋转有序数组中的最小值

--- FindMin.h
+++ FindMin.h
@@ -69,11 +69,11 @@ void TestFindMin()
 	nums.push_back(10);*/
 
 	nums.push_back(4);
+	nums.push_back(0);
 	nums.push_back(4);
 	nums.push_back(4);
 	nums.push_back(4);
-	nums.push_back(1);
-	nums.push_back(3);
+	nums.push_back(4);
 	nums.push_back(4);
 
 
